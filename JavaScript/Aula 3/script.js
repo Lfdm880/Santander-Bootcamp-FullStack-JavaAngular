cb75a7770956f267b689cfab460150ef5e25cd06
Lucas Miranda <lfdm@poli.br>
2023-08-25 08:19:54 -0300

    alterções no txt e script

--- JavaScript/Aula 3/script.js
+++ JavaScript/Aula 3/script.js
@@ -1,2 +1,5 @@
-let array =['string',1,true];
-console.log(array);
+//let array =['string',1,true];
+//console.log(array);
+
+let array2 =['casa',3,false]; //saber o valor do indice
+console.log(array2(2));
