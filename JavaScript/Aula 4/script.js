1660ef473d177ac11bdf18f58a8e3a17334cf1e0
Lucas Miranda <lfdm@poli.br>
2023-08-25 09:42:02 -0300

    encerrada aula de estrutura condicionais

--- JavaScript/Aula 4/script.js
+++ JavaScript/Aula 4/script.js
@@ -1,25 +1,58 @@
-var jogador1 = 0;
-var jogador2 = 0;
-var placar;
+// var jogador1 = 0;
+// var jogador2 = 0;
+// var placar;
 
-jogador1 != -1 && jogador2 != -1 ? console.log('os jogadores sao validos') : console.log('jogadores inválidos');
-if (jogador1 > 0) {
-    console.log('jogador 1 marcou ponto')
-} else if (jogador2 > 0) {
-    console.log('jogador 2 marcou ponto')
-}
-else {
-    console.log('Ninguém nao marcou')
-}
+// jogador1 != -1 && jogador2 != -1 ? console.log('os jogadores sao validos') : console.log('jogadores inválidos');
+// if (jogador1 > 0) {
+//     console.log('jogador 1 marcou ponto')
+// } else if (jogador2 > 0) {
+//     console.log('jogador 2 marcou ponto')
+// }
+// else {
+//     console.log('Ninguém nao marcou')
+// }
 
-switch (placar) {
-    case placar = jogador1 > jogador2:
-        console.log('jogador 1 ganhou');
-        break
-    case placar = jogador2 > jogador1:
-        console.log('jogador 1 ganhou');
+// switch (placar) {
+//     case placar = jogador1 > jogador2:
+//         console.log('jogador 1 ganhou');
+//         break
+//     case placar = jogador2 > jogador1:
+//         console.log('jogador 1 ganhou');
 
-        break
+//         break
 
     
-}
+// }
+
+let array = [ 'valor1', 'valor2', 'valor3', 'valor4', 'valor5'];
+
+let objetct = {propriedade1: 'valor1', propriedade2: 'valor2', propriedade3:'valor3'};
+
+//for executa uma instrução at~e que ela seja falsa
+
+//  for(let i = 0; i < array.length; i++){
+
+//     console.long(i);
+
+//  }
+// for(let i in array){
+//     console.log(i);
+
+// }
+
+// for(let i of array){
+//     console.log(i);
+
+// }
+
+ var a=0;
+
+//  while(a < 10){
+//      a++;
+//     console.log(a);
+//  }
+
+ do {
+    a++;
+    console.log(a);
+ }while(a<20)
