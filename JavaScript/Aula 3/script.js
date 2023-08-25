a0e20d291c3c075889274403cb53b19b4f6147dc
Lucas Miranda <lfdm@poli.br>
2023-08-25 08:41:48 -0300

    adicionada alterações

--- JavaScript/Aula 3/script.js
+++ JavaScript/Aula 3/script.js
@@ -1,5 +1,18 @@
 //let array =['string',1,true];
 //console.log(array);
 
-let array2 =['casa',3,false]; //saber o valor do indice
-console.log(array2(2));
+// let array2 = ['casa',3,false]; //saber o valor do indice
+
+// array2.push("teste array")
+//array2.pop()
+//array2.shift()
+// console.log(array2);
+
+let object = { string: 'Lucas', idade:'25 anos', Bolean:'true', array:["array"]};
+console.log(object.string);
+
+var string = object.string;
+console.log(string);
+
+var idade = object.idade;
+console.log(idade);
